6085c6048ccee06df9907ab6af7b832c1b019a9d
Alexander Nookinov <anookinov@gmail.com>
2021-08-22 00:08:31 +0700

    add lifetime annotations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    {
+        // let r;
+
+        {
+            let x = 5;
+            let r = &x;
+            println!("r: {}", r);
+        }
+
+        // println!("r: {}", r);
+    }
+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
 }
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+// lifetime annotations
+// &i32        // a reference
+// &'a i32     // a reference with an explicit lifetime
+// &'a mut i32 // a mutable reference with an explicit lifetime
