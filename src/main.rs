65d199e4dc52f4caa344c98df4086f146c1f94e2
Alexander Nookinov <anookinov@gmail.com>
2021-08-22 00:55:33 +0700

    add lifetime annotations in struct definitions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
     {
+        let novel = String::from("Call me Ishmael. Some years ago...");
+        let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+        let i = ImportantExcerpt {
+            part: first_sentence,
+        };
+        println!("{:#?}", i);
+
         // let r;
 
         {
@@ -16,6 +28,22 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+
+    // let string1 = String::from("long string is long");
+    // let result;
+    // {
+    //     let string2 = String::from("xyz");
+    //     result = longest(string1.as_str(), string2.as_str());
+    // }
+    // println!("The longest string is {}", result);
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
@@ -25,6 +53,16 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
         y
     }
 }
+
+fn longest_x<'a>(x: &'a str, y: &str) -> &'a str {
+    x
+}
+
+// cannot because of a dangling reference to local variable reference
+// fn longest_no<'a>(x: &str, y: &str) -> &'a str {
+//     let result = String::from("really long string");
+//     result.as_str()
+// }
 
 // lifetime annotations
 // &i32        // a reference
